ada2d4ef8551b7019ff32c7bcb7cc13bbd629611
Nakita <nakita.strangeways@numerator.com>
2020-03-28 18:22:55 -0700

    changed password - maybe work?

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramnstrange",
+    "password": "udagramnstrange",
+    "database": "udagramnstrange",
+    "host": "udagramnstrange.cj6xemhpolby.us-west-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-west-1",
     "aws_profile": "default",
     "aws_media_bucket": "udagram-ruttner-dev"
   },
